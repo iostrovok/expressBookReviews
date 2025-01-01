e26664b86699ab8dd58cbf82526ab5cd4a3e4ce3
vlad-clutch <vladimir@clutch.co>
2025-01-01 16:41:58 -0500

    author search

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -29,8 +29,20 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const author = req.params.author;
+
+    let res = [];
+    for (let [key, book] of Object.entries(books)) {
+        if (author === book.author) {
+            res.push(book);
+        }
+    }
+
+    if (res.length === 0) {
+        return res.status(404).json({});
+    }
+
+    return res.status(200).json(res);
 });
 
 // Get all books based on title
