e941983ebfe980768560d8fb852223832f3d3d6f
vlad-clutch <vladimir@clutch.co>
2025-01-01 16:32:22 -0500

    isbn search + books data

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -10,16 +10,22 @@ public_users.post("/register", (req,res) => {
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
-// Get the all books as  list available in the shop
+// Get the all books as list available in the shop
 public_users.get('/',function (req, res) {
     return res.status(200).json(books);
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+    const isbn = req.params.isbn;
+    const book = books[isbn];
+
+    if (!book) {
+        return res.status(404).json({});
+    }
+
+    return res.status(200).json(book);
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
