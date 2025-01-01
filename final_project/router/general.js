2702b34a6720c9adfc8700ff25690bca0c9829b9
vlad-clutch <vladimir@clutch.co>
2025-01-01 16:47:37 -0500

    author title

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -47,8 +47,20 @@ public_users.get('/author/:author',function (req, res) {
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const title = req.params.title;
+
+    let out = [];
+    for (let [key, book] of Object.entries(books)) {
+        if (title === book.title) {
+            out.push(book);
+        }
+    }
+
+    if (out.length === 0) {
+        return res.status(404).json({});
+    }
+
+    return res.status(200).json(out);
 });
 
 //  Get book review
